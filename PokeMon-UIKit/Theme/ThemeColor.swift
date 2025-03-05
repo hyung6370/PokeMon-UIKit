5442c4de3ed7806f4e480b4dcb077906dd165ad9
Hyungjun KIM <rlagudwns886@gmail.com>
2025-03-05 16:53:24 +0900

    ✨Feat: 위젯 컬러 데이터 대로 바꾸기(#4)

--- PokeMon-UIKit/Theme/ThemeColor.swift
+++ PokeMon-UIKit/Theme/ThemeColor.swift
@@ -6,27 +6,28 @@
 //
 
 import UIKit
+import SwiftUI
 
 struct ThemeColor {
-    static let primary  = UIColor(hexString: "D21312")
-    static let normal   = UIColor(hexString: "D8D9DA")
-    static let fighting = UIColor(hexString: "F28705")
-    static let flying   = UIColor(hexString: "95C8FF")
-    static let poison   = UIColor(hexString: "9652D9")
-    static let ground   = UIColor(hexString: "AA7939")
-    static let rock     = UIColor(hexString: "BCB889")
-    static let bug      = UIColor(hexString: "9FA423")
-    static let ghost    = UIColor(hexString: "6E4570")
-    static let steel    = UIColor(hexString: "6AAED3")
-    static let fire     = UIColor(hexString: "FF612B")
-    static let water    = UIColor(hexString: "2892FF")
-    static let grass    = UIColor(hexString: "47BF26")
-    static let electric = UIColor(hexString: "F2CB05")
-    static let psychic  = UIColor(hexString: "FF637F")
-    static let ice      = UIColor(hexString: "62DFFF")
-    static let dragon   = UIColor(hexString: "5462D6")
-    static let dark     = UIColor(hexString: "4F4747")
-    static let fairy    = UIColor(hexString: "FFB1FF")
+    static let primary  = UIColor(hex: "D21312")
+    static let normal   = UIColor(hex: "D8D9DA")
+    static let fighting = UIColor(hex: "F28705")
+    static let flying   = UIColor(hex: "95C8FF")
+    static let poison   = UIColor(hex: "9652D9")
+    static let ground   = UIColor(hex: "AA7939")
+    static let rock     = UIColor(hex: "BCB889")
+    static let bug      = UIColor(hex: "9FA423")
+    static let ghost    = UIColor(hex: "6E4570")
+    static let steel    = UIColor(hex: "6AAED3")
+    static let fire     = UIColor(hex: "FF612B")
+    static let water    = UIColor(hex: "2892FF")
+    static let grass    = UIColor(hex: "47BF26")
+    static let electric = UIColor(hex: "F2CB05")
+    static let psychic  = UIColor(hex: "FF637F")
+    static let ice      = UIColor(hex: "62DFFF")
+    static let dragon   = UIColor(hex: "5462D6")
+    static let dark     = UIColor(hex: "4F4747")
+    static let fairy    = UIColor(hex: "FFB1FF")
     
     static func typeColor(type: PokemonType) -> UIColor {
         switch type {
@@ -65,5 +66,57 @@ struct ThemeColor {
         case .fairy:
             return ThemeColor.fairy
         }
+    }
+    
+    static func typeColorSwiftUI(type: PokemonType) -> Color {
+        switch type {
+        case .normal:   return Color(hex: "D8D9DA").opacity(0.6)
+        case .fighting: return Color(hex: "F28705").opacity(0.6)
+        case .flying:   return Color(hex: "95C8FF").opacity(0.6)
+        case .poison:   return Color(hex: "9652D9").opacity(0.6)
+        case .ground:   return Color(hex: "AA7939").opacity(0.6)
+        case .rock:     return Color(hex: "BCB889").opacity(0.6)
+        case .bug:      return Color(hex: "9FA423").opacity(0.6)
+        case .ghost:    return Color(hex: "6E4570").opacity(0.6)
+        case .steel:    return Color(hex: "6AAED3").opacity(0.6)
+        case .fire:     return Color(hex: "FF612B").opacity(0.6)
+        case .water:    return Color(hex: "2892FF").opacity(0.6)
+        case .grass:    return Color(hex: "47BF26").opacity(0.6)
+        case .psychic:  return Color(hex: "FF637F").opacity(0.6)
+        case .ice:      return Color(hex: "62DFFF").opacity(0.6)
+        case .dragon:   return Color(hex: "5462D6").opacity(0.6)
+        case .dark:     return Color(hex: "4F4747").opacity(0.6)
+        case .fairy:    return Color(hex: "FFB1FF").opacity(0.6)
+        }
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        
+        let red = Double((rgbValue >> 16) & 0xFF) / 255.0
+        let green = Double((rgbValue >> 8) & 0xFF) / 255.0
+        let blue = Double(rgbValue & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+}
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb >> 16) & 0xFF) / 255.0
+        let green = CGFloat((rgb >> 8) & 0xFF) / 255.0
+        let blue = CGFloat(rgb & 0xFF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
--- PokeMonWidget/PokemonWidgetEntryView.swift
+++ PokeMonWidget/PokemonWidgetEntryView.swift
@@ -11,39 +11,34 @@ struct PokemonWidgetEntryView: View {
     var entry: PokemonProvider.Entry
     
     var body: some View {
-        ZStack {
-            if let pokemon = entry.pokemon {
-                VStack {
-                    if let cachedImage = PokeMonWidgetManager.shared.getCachedImage(for: pokemon.imageUrl) {
-                        Image(uiImage: cachedImage)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 80)
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                    } else {
-                        Image(systemName: "photo")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 80)
-                            .foregroundColor(.gray)
-                    }
-
-                    Text(pokemon.koreanName ?? "")
-                        .font(.headline)
-                        .bold()
-                        .lineLimit(1)
-                    
-                    Text("#\(pokemon.id)")
-                        .font(.caption)
+        if let pokemon = entry.pokemon {
+            VStack {
+                if let cachedImage = PokeMonWidgetManager.shared.getCachedImage(for: pokemon.imageUrl) {
+                    Image(uiImage: cachedImage)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 80, height: 80)
                         .foregroundColor(.gray)
                 }
-                .widgetBackground(.white)
-                .padding()
-            } else {
-                Text("포켓몬 정보를 불러오는 중...")
+
+                Text(pokemon.koreanName ?? "")
+                    .font(.headline)
+                    .bold()
+                    .lineLimit(1)
+                
+                Text("#\(pokemon.id)")
+                    .font(.caption)
+                    .foregroundColor(.gray)
             }
+            .widgetBackground(ThemeColor.typeColorSwiftUI(type: pokemon.pokemonTypes.first ?? .normal))
+        } else {
+            Text("포켓몬 정보를 불러오는 중...")
         }
-        .padding()
-        .widgetBackground(.white)
     }
 }
